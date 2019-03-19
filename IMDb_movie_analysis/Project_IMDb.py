8da25224c70070f4e96d0c86986a2e7304dab5bd
Shengzhao_Xia <shengzhao.xia@epfl.ch>
2019-03-19 17:46:37 +0100

    Upload Network files

---
+++ IMDb_movie_analysis/Project_IMDb.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 21:59:53 2019
+
+@author: Gentle Deng
+"""
+
+import numpy as np
+
+from Lib_graph import construct_graphs
+from Lib_prediction import ROI_prediction
+from Lib_gradientdecent import Optimize_GraphWeight
+from Lib_vis import visual_list, visual_signalongraph
+
+# In[ PART 1. Generate subgraphs ]
+
+# 1.setting file path and choosing features
+Genre     = 'Science Fiction'
+File_path = r'./data'
+features  = ['budget',
+             'cast',
+             'crew',
+             'genres',    # but genre is not used to build a subgraph up to now
+             'keywords',
+             'popularity',
+             'production_companies',
+             'vote_average']
+
+# 2.constructing subgraphs
+signal, W_3d = construct_graphs( File_path, features, genre = Genre)
+
+
+# In[ PART 2. Optimize weight vector V ] 
+
+# 1.initialize GD parameters
+maxiters        = 200
+step_Vk         = 0.2
+step_t          = 0.00000005
+Lambda          = 0.01     # L1 regularization lambda = 0.01
+gain            = 5
+
+algo_reg        = 'L2'
+algo_grad       = 'matrix'
+adaptive_step   = True
+
+# 2.initialize kernel parameters
+# It would be more convincing that weights of these subgraphs start from the same 
+# start point (1/number of graphs). In addition, our objective funtion is nonconvex 
+# to Vk. Thus if Vk is randomly initialized, it tends to converge to different local minima.
+t               = 0.001
+Vk              = 1/( len(features) - 1 ) * np.ones( len(features) - 1 )  # genre is not used to build a subgraph up to now
+
+# 3.iteratively optimize Vk
+t_list, Vk_list, loss_list, final_weight = Optimize_GraphWeight( W_3d,      Vk,       signal, t,       gain,   maxiters,
+                                                                 algo_grad, algo_reg, Lambda, step_Vk, step_t, adaptive_step)
+# 4.save weight vector list
+np.save(File_path + '/Vk_list_' + Genre, Vk_list)
+
+# In[ PART 3. Visualize the results ] 
+
+# 1.visualze the process of optimization 
+legend = features.copy()
+legend.remove('genres')
+visual_list( loss_list, t_list, Vk_list, Vk_name = legend, save = True)
+
+# 2.visualize the signal on the final graph 
+visual_signalongraph(final_weight, signal, labels = np.ones(final_weight.shape[0]), save = True)
+
+# 3.visualize the signal on the original graph
+Weight = np.zeros(W_3d.shape[0:2])
+for ct in range( len(Vk) ):
+    Weight += W_3d[:,:,ct] * Vk[ct]
+visual_signalongraph(Weight, signal, labels = np.ones(Weight.shape[0]), save = True)
+
+
+# In[ PART 4. Predict the ROI of a given new node ]
+
+# 1. define a new node with its 
+# Please fill None when there is no data in a feature
+La_La_Land = { # La La Land, revenue: 446092357
+             'budget':      30000000, 
+             'cast':        ['Ryan Gosling', 'Emma Stone', 'Amiée Conn', 'Terry Walters', 'Thom Shelton'], 
+             'crew':        'Damien Chazelle', 
+             'genres':      None, 
+             'keywords':    ['los angeles california', 'pianist', 'aspiring actress', 'musician', 'jazz club'], 
+             'popularity':  284, 
+             'production_companies':['Summit Entertainment', 'Black Label Media', 'TIK Films', 'Impostor Pictures', 'Gilbert Films', 'Marc Platt Productions'], 
+             'vote_average':8.0}
+
+Pacific_Rim2 = { # revenue: 290061297
+            'budget':      150000000 , 
+             'cast':        ['Jake Pentecost', 'Nate Lambert', 'Amara Namani', 'Hermann Gottlieb'], 
+             'crew':        'Steven S. DeKnight', 
+             'genres':      None, 
+             'keywords':    ['sea doo', 'woman with masculine short hair', 'woman with masculine hair', 'chinawoman', 'teen girl with too much make up'], 
+             'popularity':  30 , 
+             'production_companies':['Clear Angle Studios', 'Dentsu', 'Double Dare You (DDY)', 'Double Negative (DNEG)', 'Fuji Television Network ', 'Legendary Entertainment'], 
+             'vote_average':5.6  }
+
+Interstellar = { # revenue: 675120017
+             'budget':      165000000 , 
+             'cast':        ['Matthew McConaughey', 'Ellen Burstyn', 'Mackenzie Foy', 'John Lithgow'], 
+             'crew':        'Christopher Nolan', 
+             'genres':      'Science Fiction', 
+             'keywords':    ['saving the world', 'artificial intelligence', 'father son relationship', 'single parent', 'nasa'], 
+             'popularity':  724.247784 , 
+             'production_companies':['Paramount Pictures', 'Legendary Pictures', 'Warner Bros.', 'Syncopy', 'Lynda Obst Productions'], 
+             'vote_average':8.1  }
+
+# 2. predict ROI of the movie
+# k-nearest neigbor to do prediction, different genres need different k (a hyperparameter) 
+#   Action = 10, Romance = 5, Science fiction = 3
+k_nn = 10
+
+Predict_ROI = ROI_prediction(La_La_Land, signal, Vk, features, k_nn, File_path, genre=Genre)
+print('The prediction on ROI of the given movie is:', Predict_ROI)
---
+++ IMDb_movie_analysis/README.md
@@ -0,0 +1,83 @@
+# How to Invest in Movies
+**Authors:** [Sheng ZhaoXia](https://github.com/SXiaZr), [Yinan Zhang](https://github.com/yinanzhangepfl), [Yu-Ting Huang](https://github.com/yuyuvalerie), [Zhantao Deng](https://github.com/GentleDell) 
+
+**Report**: [How to Invest in Movies](https://github.com/GentleDell/IMDb_movie_analysis/blob/master/IMDB_Project.pdf)
+
+## Abstract
+
+
+## 1. Prerequisites
+This project is based on [Python](https://www.python.org/) and we use [Spyder](https://www.spyder-ide.org/) as our python programming environment. We also provide `.ipynb` files for [Jupyter notebook](https://jupyter.org/). These softwares can be installed together with [Anaconda](https://www.anaconda.com/). In addition, We install all packages through the Anaconda Prompt. These packages have been tested in **Window 10 Home** and **macOS Mojave**, but it should be easy to implement in other platforms. 
+
+### Anaconda
+This project is based on anaconda and jupiter notebook. Download and install instructions can be found at: https://www.anaconda.com/download/. After installing Anaconda, `pip` and `conda` can be used to install Python packages. Spyder will also be installed together with 
+
+### Scipy
+We use the scientific computing and visualization functionalities of [scipy](https://www.scipy.org/install.html), especially the numpy, pandas and matplotlib package. These packages can be installed by typing the following command in your Anaconda Prompt.
+```
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+```
+
+### Networkx
+We use [networkx](http://networkx.github.io) to visualize signals and graph structure. The package can be installed by typing the following command in your Anaconda Prompt.
+```
+pip install networkx
+```
+
+### NLTK
+We use [NLTK](https://www.nltk.org/) to analyze the keywords of each movie. The package can be installed by typing the following command in your Anaconda Prompt.
+```
+pip install -U nltk
+```
+
+## 2. Research questions
+In this project we have figured out:
+- What factors are important for the ROI (Return on Investment) of a specific genre?
+- How to predict the ROI of a new movie with some prior features, e.g. budget and production companies ?
+
+## 3. Dataset
+### Original Dataset:
+We use the dataset on [Kaggle](https://www.kaggle.com/tmdb/tmdb-movie-metadata). It contains two csv files: 
+- `tmdb_5000_credits.csv` (~ 38.1MB): 
+
+The dataset contains the cast and crew data of 4803 movies. The cast feature mainly depicts what actors appear in the movie and what characters they star. So as the crew data, which descibles the names and jobs of specific crews. 
+- `tmdb_5000_movies.csv` (~ 5.43MB): 
+
+The dataset contains 4803 movies data with 20 distinct features. The important features that we are interested: budget, genres ,keywords, title, popularity, production_companies, revenue, vote_average.
+
+
+
+## 4. What we have done
+### Auxiliary modules:
+- `Lib_gradientdecent.py`: contains gradient decent algorithms of ridge regression and lasso regression.
+- `Lib_graph.py`: contains functions of constructing all subgraphs.
+- `Lib_keywords.py`: contains fuctions grouping keywords by word roots and synonym and training a word2vec model.
+- `Lib_prediction.py`: contains functions to predict return of investment according movie network.
+- `Lib_vis.py`: contains all kinds of visualization functions. 
+
+### Working modules:
+- `Lib_actor_network.ipynb`: explores actor data. Construct actor social network and visualized it via networkx package.
+- `Project_IMDb.py`: a Python interface. A pipeline to run whole codes of our project from data exploration, feature preprocessing to subgraphs construction, ML model training and prediction. 
+- `Project_IMDb.jpynb`: a Jupyter notebook interface to run our project. It shares almost the same codes with `Project_IMDb.py`.
+- `Testing.py`: a Python interface for those who want to predict ROI of a new moive using pre-trained model. 
+
+## 5. Contributions of group members
+**Sheng ZhaoXia:**
+- Actor/ress social network analysis
+- Genre and keyword subgraphs
+- Reports
+
+**Yinan Zhang:**
+- NLTK based keywords analysis and processing 
+- Actor/ress and director subgraphs
+- Reports
+
+**Yuting Huang:**
+- Budget and popularity subgraphs
+- ROI (Return on Investment) signal
+- Gradient decent and report
+
+**Zhantao Deng:**
+- Ridge regression, LASSO and their gradient decent
+- Average vote and production companies subgraphs
+- Report
